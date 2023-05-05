1470a61fc458774f93baad3c5a50e11e04f9d30f
Jack Harter <jack.harter@me.com>
2023-05-05 16:30:13 -0500

    formatted

--- main.tf
+++ main.tf
@@ -20,13 +20,13 @@ module "vpc" {
   name = "hartrnet-dev-vpc"
   cidr = "10.0.0.0/16"
 
-  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  azs            = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  public_subnets = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   enable_nat_gateway = true
 
   tags = {
-    Terraform = "true"
+    Terraform   = "true"
     Environment = "dev"
   }
 }
@@ -35,7 +35,7 @@ resource "aws_instance" "blob" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
-  vpc_security_group_ids =[module.blob-sg.security_group_id]
+  vpc_security_group_ids = [module.blob-sg.security_group_id]
 
   tags = {
     Name = "HelloWorld"
@@ -45,12 +45,12 @@ resource "aws_instance" "blob" {
 module "blob-sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "4.17.2"
-  name = "blob_security_group"
+  name    = "blob_security_group"
 
-  vpc_id = module.vpc.public_subnets[0]
-  ingress_rules = ["http-80-tcp", "https-443-tcp"]
+  vpc_id              = module.vpc.public_subnets[0]
+  ingress_rules       = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
 
-  egress_rules = ["all-all"]
+  egress_rules       = ["all-all"]
   egress_cidr_blocks = ["0.0.0.0/0"]
 }

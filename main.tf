50cdaf695d48f6b184c7507555edaa4eec50fedf
Jack Harter <jack.harter@me.com>
2023-05-05 17:04:01 -0500

    maybe a public/private subnet issue

--- main.tf
+++ main.tf
@@ -23,7 +23,7 @@ module "blob_vpc" {
   azs            = ["us-west-2a", "us-west-2b", "us-west-2c"]
   public_subnets = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
-  enable_nat_gateway = false
+  enable_nat_gateway = true
 
   tags = {
     Terraform   = "true"
@@ -34,7 +34,7 @@ module "blob_vpc" {
 resource "aws_instance" "blob" {
   ami                    = data.aws_ami.app_ami.id
   instance_type          = var.instance_type
-  subnet_id              = module.blob_vpc.public_subnets[0]
+  subnet_id              = module.blob_vpc.private_subnets[0]
   vpc_security_group_ids = [module.blob_sg.security_group_id]
 
   tags = {

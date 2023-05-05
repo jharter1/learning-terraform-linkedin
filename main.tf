7e11b1780708a9ee22f73ae6ec938834b2793a12
Jack Harter <jack.harter@me.com>
2023-05-05 17:42:50 -0500

    hotfix

--- main.tf
+++ main.tf
@@ -66,12 +66,6 @@ module "blob_alb" {
       backend_protocol = "HTTP"
       backend_port     = 80
       target_type      = "instance"
-      targets = {
-        my_target = {
-          target_id = aws_instance.blob.id
-          port = 80
-        }
-      }
     }
   ]
 

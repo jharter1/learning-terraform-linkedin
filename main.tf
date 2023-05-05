7c6e4249993f76986858fe83eefb1c163add0d9f
Jack Harter <jack.harter@me.com>
2023-05-05 18:15:34 -0500

    variablizing

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ data "aws_ami" "app_ami" {
 
   filter {
     name   = "name"
-    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
+    values = [var.ami_filter.name]
   }
 
   filter {
@@ -11,23 +11,23 @@ data "aws_ami" "app_ami" {
     values = ["hvm"]
   }
 
-  owners = ["979382823631"] # Bitnami
+  owners = [var.ami_filter.owner]
 }
 
 module "blob_vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name = "hartrnet-dev"
-  cidr = "10.0.0.0/16"
+  name = "hartrnet-${var.environment.name}"
+  cidr = "${var.environment.network_prefix}.0.0/16"
 
   azs            = ["us-west-2a", "us-west-2b", "us-west-2c"]
-  public_subnets = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  public_subnets = ["${var.environment.network_prefix}.101.0/24", "${var.environment.network_prefix}.102.0/24", "${var.environment.network_prefix}.103.0/24"]
 
   enable_nat_gateway = false
 
   tags = {
     Terraform   = "true"
-    Environment = "dev"
+    Environment = "${var.environment.name}"
   }
 }
 
@@ -36,8 +36,8 @@ module "autoscaling" {
   version = "6.9.0"
   # insert the 1 required variable here
   name     = "blob"
-  min_size = 1
-  max_size = 3
+  min_size = var.asg_min_size
+  max_size = var.asg_max_size
 
   vpc_zone_identifier = module.blob_vpc.public_subnets
   target_group_arns   = module.blob_alb.target_group_arns
@@ -78,14 +78,14 @@ module "blob_alb" {
   ]
 
   tags = {
-    Environment = "dev"
+    Environment = "${var.environment.name}"
   }
 }
 
 module "blob_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "4.13.0"
-  name    = "blob"
+  name    = "${var.environment.name}_blob"
 
   vpc_id              = module.blob_vpc.vpc_id
   ingress_rules       = ["http-80-tcp", "https-443-tcp"]
--- variables.tf
+++ variables.tf
@@ -2,3 +2,39 @@ variable "instance_type" {
   description = "Type of EC2 instance to provision"
   default     = "t3.nano"
 }
+
+variable "ami_filter" {
+  type = object({
+    name = string
+    owner = string
+  })
+
+  default = {
+  name = "bitnami-tomcat-*-x86_64-hvm-ebs-nami"
+  owner = "979382823631" # Bitnami
+  }
+}
+
+variable "environment" {
+  description = "development environment"
+  
+  type = object({
+    name = string
+    network_prefix = string
+  })
+  default = {
+  name = "hartrnet-dev"
+  network_prefix = "10.0"
+  }
+
+}
+
+variable "asg_min_size" {
+  description = "Minimum number of instances to provision in the ASG"
+  default = 1
+}
+
+variable "asg_max_size" {
+  description = "Maximum number of instances to provision in the ASG"
+  default = 3
+}

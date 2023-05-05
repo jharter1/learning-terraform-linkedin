e825bac6d5002a7ea4d23384d20a9cdec22066ca
Jack Harter <jack.harter@me.com>
2023-05-05 16:03:53 -0500

    new model for vpc sg ids

--- main.tf
+++ main.tf
@@ -22,11 +22,24 @@ resource "aws_instance" "blob" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
-  vpc_security_group_ids =[aws_security_group.blob.id]
+  vpc_security_group_ids =[module.blob-sg.security_group_id]
 
   tags = {
     Name = "HelloWorld"
   }
+}
+
+module "blob-sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "4.17.2"
+  name = "blob_new_security_group"
+
+  vpc_id = data.aws_vpc.default.id
+  ingress_rules = ["http-80-tcp", "https-443-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+
+  egress_rules = ["all-all"]
+  egress_cidr_blocks = ["0.0.0.0/0"]
 }
 
 resource "aws_security_group" "blob" {
